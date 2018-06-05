7ba0a0589b207c34e7b002e2898b17b964157735
Birkir Bjornsson <birkirbjornsson@laptop-97i28qug.kea.local>
2018-06-05 10:46:35 +0200

    breytingar

--- main.js
+++ main.js
@@ -5,32 +5,32 @@ const form = document.querySelector("form");
 const template = document.querySelector("#noteTemplate").content;
 
 //add new notes
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    
-   // 
-    
-    database.ref("notes/").push({
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+     database.ref("notes/").push({
         header: headerEl.value,
         description: descriptionEl.value
     });
 
     // clear out form
-    headerEl.value="";
-    descriptionEl.value="";
+    headerEl.value = "";
+    descriptionEl.value = "";
 });
-
-database.ref("notes/").on("child_added", (snapshot)=>{
-   
-    const key = snapshot.key;
-    const data = snapshot.val();
-
-    const clone = template.cloneNode(true);
+// listen for new data
+database.ref("notes/").on("child_added", (snapshot) => {
+const key = snapshot.key;
+const data = snapshot.val();
+const clone = template.cloneNode(true);
+    clone.querySelector("article").dataset.key=key;
     clone.querySelector("h1").textContent = data.header;
     clone.querySelector("div.description").textContent = data.description;
     app.appendChild(clone);
 });
 
-database.ref("notes/").on("child_removed", snapshot=>{
-    console.log(snapshot.key, snapshot.val());
+// listen for removal of data child_removed
+database.ref("notes/").on("child_removed", snapshot => {
+    const key = snapshot.key;
+    let el = document.querySelector(`article[data-key=${key}]`);
+    el.remove();
+   
 });

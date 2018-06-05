acb53814ddd2699488f797050692fc0e289acfe6
Birkir Bjornsson <birkirbjornsson@laptop-97i28qug.kea.local>
2018-06-05 13:20:16 +0200

    ready and functioning

--- main.js
+++ main.js
@@ -24,6 +24,37 @@ const clone = template.cloneNode(true);
     clone.querySelector("article").dataset.key=key;
     clone.querySelector("h1").textContent = data.header;
     clone.querySelector("div.description").textContent = data.description;
+    clone.querySelector("button.delete").addEventListener("click",e=>{
+        database.ref("notes/"+key).remove();
+    })
+
+    clone.querySelector("button.edit").addEventListener("click", e=>{
+        const formClone = document.querySelector("#editFormTemplate").content;
+        const theArticle = e.target.parentElement.parentElement;
+        const theDiv = theArticle.querySelector("div").textContent;
+        const theHeader = theArticle.querySelector("h1").textContent;
+        const theKey = theArticle.dataset.key;
+        console.log(theDiv, theHeader, theKey);
+        formClone.querySelector("input").value=theHeader;
+        formClone.querySelector("textarea").value=theDiv;
+        formClone.querySelector("form").addEventListener("submit", e=>{
+                e.preventDefault();
+                let lastForm = document.querySelector("footer+form")
+                database.ref("notes/"+theKey).set({
+                    header: lastForm.querySelector("input").value,
+                    description: lastForm.querySelector("textarea").value
+                })
+
+                lastForm.remove();
+                theArticle.querySelector("h1").textContent=lastForm.querySelector("input").value;
+                theArticle.querySelector("div").textContent=lastForm.querySelector("textarea").value;
+                //windows.location="" works...not nice
+                //we should probably use child_changed
+
+        })
+          
+        theArticle.appendChild(formClone);
+    });
     app.appendChild(clone);
 });
 
@@ -34,3 +65,4 @@ database.ref("notes/").on("child_removed", snapshot => {
     el.remove();
    
 });
+

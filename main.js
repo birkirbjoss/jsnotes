d6e35b8bbf17ca7172bdc42b020af6236674653b
Birkir Bjornsson <birkirbjornsson@laptop-97i28qug.kea.local>
2018-06-05 10:27:02 +0200

    hi mom

---
+++ main.js
@@ -0,0 +1,32 @@
+const database = firebase.database();
+const headerEl = document.querySelector("#header");
+const descriptionEl = document.querySelector("#description");
+const form = document.querySelector("form");
+const template = document.querySelector("#noteTemplate").content;
+
+//add new notes
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+   // 
+    console.log(headerEl.value)
+    database.ref("notes/").push({
+        header: headerEl.value,
+        description: descriptionEl.value
+    });
+
+    // clear out form
+    headerEl.value="";
+    descriptionEl.value="";
+});
+
+database.ref("notes/").on("child_added", (snapshot)=>{
+    console.log(snapshot);
+    const key = snapshot.key;
+    const data = snapshot.val();
+
+    const clone = template.cloneNode(true);
+    clone.querySelector("h1").textContent = data.header;
+    clone.querySelector("div.description").textContent = data.description;
+    app.appendChild(clone);
+})
